1886a1785d7e385d37cd6b686c7beb71c1fbc4ba
Xin Zhang <xinzhang1215@gmail.com>
2019-07-11 22:02:14 +0800

    polish theme

--- static/js/main.js
+++ static/js/main.js
@@ -47,6 +47,16 @@ if (location.hash) {
     $(window).trigger('hashchange');
 }
 
+function replyTo(parent, name) {
+  var e = document.getElementById(parent),
+      f = document.getElementById('comment-form'),
+      h = document.getElementById('comment-form-header');
+
+  h.innerHTML = 'Reply to ' + name;
+  e.parentNode.insertBefore(f, e.nextSibling);
+  document.getElementsByName('fields[reply_to]')[0].value=parent;
+}
+
 // // $('[data-spy="scroll"]').each(function () {
 // //     var $spy = $(this).scrollspy('refresh')
 // //   })
